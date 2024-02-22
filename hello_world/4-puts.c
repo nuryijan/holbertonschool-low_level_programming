8fd0d4724c8af73ca7c79297202c3385d376bdf9
nuryijan <nur.navarro@gmail.com>
2024-02-22 19:25:07 +0000

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints "programming is like bulding a multilingual 
+ * puzzle, followed by a new line."
+ * return 0.
+ */
+int main (void)
+
+{
+	puts ("\"programming is like building a multilingual puzzle, followed by a new line");
+
+	return (0);
+}
+
