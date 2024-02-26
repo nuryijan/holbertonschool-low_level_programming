5892c53d97bf83fff50c402a45ac3c5423e04ddf
nuryijan <nur.navarro@gmail.com>
2024-02-26 18:54:23 +0000

    5-print.c

---
+++ hello_world/5-print.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+ * main - a program that print a line using the printf fuction
+ * Return: 0(success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (-1);
+}
+
